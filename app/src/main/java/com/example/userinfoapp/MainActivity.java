ec9c35ced12aa9503da786a7098c4c5aaaab19f6
saeed921 <68588036+saeed921@users.noreply.github.com>
2021-08-23 10:36:11 -0600

    //Add BottomNavigation Here and also create a menu named mainmenu.xml here,

--- app/src/main/java/com/example/userinfoapp/MainActivity.java
+++ app/src/main/java/com/example/userinfoapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.userinfoapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -10,28 +12,40 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FirebaseAuth auth=FirebaseAuth.getInstance();
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggleButton;
 
 BottomNavigationView bnv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Drawer Layout & Navigation view declaring Id
+        drawerLayout=findViewById(R.id.drawerID);
+        navigationView=findViewById(R.id.navigationID);
+        toggleButton=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.off);
+        drawerLayout.addDrawerListener(toggleButton);
+        toggleButton.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Bottom Navigation
         bnv=findViewById(R.id.bottomID);
         bnv.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull  MenuItem item) {
-
                 switch (item.getItemId()){
-
-
                     case R.id.homeID:
                         Toast.makeText(MainActivity.this, "Home Id", Toast.LENGTH_SHORT).show();
                         break;
@@ -46,22 +60,25 @@ BottomNavigationView bnv;
                         progressDialog.show();
                         progressDialog.show();
                         Toast.makeText(MainActivity.this, "list", Toast.LENGTH_SHORT).show();
-
                         break;
-
-
                 }
 
             }
         });
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull  MenuItem item) {
 
 
+        return true;
 
+    }
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
-
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater=getMenuInflater();
@@ -74,6 +91,9 @@ BottomNavigationView bnv;
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggleButton.onOptionsItemSelected(item)){
+            return  true;
+        }
         switch (item.getItemId()){
             case  R.id.logout: auth.signOut();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
